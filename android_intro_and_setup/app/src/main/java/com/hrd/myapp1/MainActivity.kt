8142b99ea27405b9bda6db649ab6802ffb5d7dd4
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2023-03-22 09:25:09 +0000

    day 2

--- android_intro_and_setup/app/src/main/java/com/hrd/myapp1/MainActivity.kt
+++ android_intro_and_setup/app/src/main/java/com/hrd/myapp1/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.hrd.myapp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val registerButton = findViewById<Button>(R.id.register_button)
+        val convertButton = findViewById<Button>(R.id.convert_button)
+        val amountInput = findViewById<EditText>(R.id.amount_input)
+        val displayValue = findViewById<TextView>(R.id.display_value)
 
-        registerButton.setOnClickListener {
-            Toast.makeText(this, "Register clicked", Toast.LENGTH_SHORT).show()
+        convertButton.setOnClickListener {
+            val amount = amountInput.text.toString().toFloat()
+            val valueInCedis = amount * 12.00
+            displayValue.text = valueInCedis.toString()
         }
     }
 }
